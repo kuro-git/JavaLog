862a6af8b388260c88a5adeb067e8ecf6bad495c
nakajimashintarou <gitgitgithub@gmail.com>
2017-12-17 22:14:35 +0900

    Humanクラスの修正

--- app/src/main/java/jp/techacademy/shintarou/nakajima/javalog/Human.java
+++ app/src/main/java/jp/techacademy/shintarou/nakajima/javalog/Human.java
@@ -13,11 +13,13 @@ class Human extends Animal implements Thinkable {
     // 変数
     String name;      // 名前
     int age;    // 年齢
+    String hobby;   // 趣味
 
     // 引数付きコンストラクタ
-    public Human(String name, int age) {
+    public Human(String name, int age, String hobby) {
         this.name = name;
         this.age = age;
+        this.hobby = hobby;
     }
 
     // クラス関数
@@ -27,12 +29,12 @@ class Human extends Animal implements Thinkable {
 
     // メソッド
     public void say() {
-        Log.d("javatest", this.name + "(" + this.age + "歳)" + "「ニャンニャン」");
+        Log.d("javatest", "私の名前は" + this.name + "です。年は" + this.age + "歳です。");
     }
 
     @Override
     public void think() {
-        Log.d("javatest", this.name + "(" + this.age + "歳)" + "は全力で愛した。");
+        Log.d("javatest", "私は" + this.hobby + "について考える。");
     }
 
 }
